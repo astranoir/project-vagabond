e05adccc30a34484a7dcd72b8689415484db1bc2
Melissa Bube <melissa.bube@inmar.com>
2016-06-15 23:28:34 -0400

    added tabs, more test data, and css file

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# project-vagabond
+# Project vagabond
+
+# Contributing
+1. Fork the project
+2. Create your feature branch (`git checkout -b my-new-feature`)
+3. Commit your changes (`git commit -am 'Add some feature'`)
+4. Push to the branch (`git push origin my-new-feature`)
+5. Create new Pull Request
--- app.js
+++ app.js
@@ -5,28 +5,74 @@
   	this.cards = tasks;
   });
 
+  app.controller('ListController', function(){
+  	this.list = 1;
+
+  	this.selectList = function(selectedList){
+  		this.list = selectedList;
+  	};
+
+  	this.isSelected = function(listSelected){
+  		return this.list === listSelected;
+  	};
+  });
+
   var tasks = [
 	  {
-	  	title: 'CAD Design for Arms',
-	  	responsible: ['student 1'],
-	  	accountable: 'student 1',
-	  	consulted: ['student 2', 'student 3'],
-	  	informed: ['student 2', 'student 3', 'student 4'],
-	  	description: 'Create CAD design for grabbing arms in SolidWorks',
-	  	status: 'Ready to be Worked',
+	  	title: 'Create rules checklist',
+	  	responsible: ['Student A', 'Student B'],
+	  	accountable: 'Student A',
+	  	consulted: ['Student C', 'Student D', 'mentor Alpha'],
+	  	informed: ['Student E', 'Student F', 'Student G', 'mentor Beta'],
+	  	description: 'Distill rules into easy-to-digest checklist',
+	  	dueDateUTC: '1288323623006',
+	  	status: 'Done',
+	  	isArchived: true,
 	  	updatedBy: 'system',
-	  	updatedAt: 'Wed, 15 Jun 2016 16:59:33 UTC'
+	  	updatedOnUTC: '1288323623006',
+	  	order: 5
+	  },
+	  {
+	  	title: 'CAD Design for Arms',
+	  	responsible: ['Student 1'],
+	  	accountable: 'Student 1',
+	  	consulted: ['Student 2', 'Student 3'],
+	  	informed: ['Student 2', 'Student 3', 'Student 4'],
+	  	description: 'Create CAD design for grabbing arms in SolidWorks',
+	  	dueDateUTC: '1288323623006',
+	  	status: 'Needs Discussion',
+	  	isArchived: false,
+	  	updatedBy: 'system',
+	  	updatedOnUTC: '1288323623006',
+	  	order: 1
 	  },
 	  {
 	  	title: 'CAD Design for Drive Train',
-	  	responsible: ['student 1'],
-	  	accountable: 'student 1',
-	  	consulted: ['student 2', 'student 3'],
-	  	informed: ['student 2', 'student 3', 'student 4'],
+	  	responsible: ['Student 1'],
+	  	accountable: 'Student 1',
+	  	consulted: ['Student 2', 'Student 3'],
+	  	informed: ['Student 2', 'Student 3', 'Student 4'],
 	  	description: 'Create CAD design for drive train in SolidWorks',
-	  	status: 'Ready to be Worked',
+	  	status: 'Ready to Be Worked',
+	  	isArchived: false,
 	  	updatedBy: 'system',
-	  	updatedAt: 'Wed, 15 Jun 2016 16:59:33 UTC'
+	  	updatedOnUTC: '1288323623006',
+	  	order: 55
+	  },
+	  {
+	  	title: 'Order food for meeting',
+	  	responsible: ['Mentor 1', 'Mentor 2'],
+	  	accountable: 'Mentor 2',
+	  	consulted: ['Mentor 12', 'Mentor 3'],
+	  	informed: ['Mentor 7', 'Student 9', 'Student 11'],
+	  	description: 'Order food; Requirements: includes meat dish, gluten-free dish, vegetarian dish',
+	  	status: 'In Progress',
+	  	budgetUSD: 125,
+	  	finalCostUSD: 123.47,
+	  	isArchived: false,
+	  	updatedBy: 'system',
+	  	updatedOnUTC: '1288323623006',
+	  	order: 0
 	  }
   ];
 })();
